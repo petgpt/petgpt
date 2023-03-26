fb2be04c7c73dd500db1554a53352099fbd04cf2
AAGA <39422824+AHGGG@users.noreply.github.com>
2023-03-26 21:25:02 +0800

    fix：build error TS6305

--- electron/main/events/ipc.ts
+++
@@ -1,8 +0,0 @@
-import { BrowserWindow, ipcMain} from 'electron'
-import {SET_MINI_WINDOW_POS} from "../../../src/utils/events/constants";
-
-
-ipcMain.on(SET_MINI_WINDOW_POS, (evt, pos) => {
-    const window = BrowserWindow.getFocusedWindow()
-    window.setBounds(pos)
-})
