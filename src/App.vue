ae43f6b16b7edfe8623bdbc19a5038464d59b6c5
AHGGG <ironpan3160899@gmail.com>
2023-03-27 16:10:20 +0800

    feat：less variables export可以在脚本中使用

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,25 @@
 <script setup lang="ts">
-  // import {useStore} from 'vuex'
-  // import {computed} from "vue";
-  //
-  // const store = useStore()
-  //
-  // const msg = computed(()=>store.state.test)
-  //
-  // setTimeout(()=>{store.commit('setTest',2)},2000) //调用mutations，修改store中的test的值
+import {useStore} from 'vuex';
+import {computed} from "vue";
+import variables from '../src/assets/css/variables.module.less';// less变量，可以在脚本中获取
+
+
+  const store = useStore()
+
+  const msg = computed(()=>store.state.test)
+
+  setTimeout(()=>{store.commit('setTest',2)},2000) //调用mutations，修改store中的test的值
 
   console.log("[App.vue]", `Hello world from Electron ${process.versions.electron}!`)
 </script>
 
 <template>
-<!--  <div class="test">vuex测试代码{{store.state.test}}</div>-->
+  <div class="test" :style="{ color: variables['greenColor'] }">vuex测试代码{{store.state.test}}</div>
   <router-view />
 </template>
 
 <style lang="less">
-//.test{
-//  background-color: @red;// less测试代码
-//}
+.test{
+  background-color: @red;// less测试代码
+}
 </style>
