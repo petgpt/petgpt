b5dc728339bf74a5db586dddf74f7185c49ec570
AAGA <ironpan3160899@gmail.com>
2023-10-05 22:32:59 +0800

    refactor: 抽离svg为函数组件

--- src/renderer/pages/chatLayout/ChatText.tsx
+++ src/renderer/pages/chatLayout/ChatText.tsx
@@ -16,6 +16,22 @@ export function MarkdownToHtml(md: string) {
   )
 }
 
+function ChatContinueSVG(props: { onClick: () => void }) {
+  return <svg
+    className="cursor-pointer"
+    onClick={props.onClick}
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+  >
+    <path
+      fill="currentColor"
+      d="M12 12.575q-.2 0-.375-.062T11.3 12.3L6.7 7.7q-.275-.275-.288-.688T6.7 6.3q.275-.275.7-.275t.7.275l3.9 3.875L15.9 6.3q.275-.275.688-.287t.712.287q.275.275.275.7t-.275.7l-4.6 4.6q-.15.15-.325.213t-.375.062Zm0 6q-.2 0-.375-.062T11.3 18.3l-4.6-4.6q-.275-.275-.288-.687T6.7 12.3q.275-.275.7-.275t.7.275l3.9 3.875l3.9-3.875q.275-.275.688-.288t.712.288q.275.275.275.7t-.275.7l-4.6 4.6q-.15.15-.325.213t-.375.062Z"
+    />
+  </svg>;
+}
+
 const ChatText = (props: ChatTextProps, ref: Ref<{
   upsertLatestText: (message: ChatItem) => void,
   clearChatContext: (isClearContext: boolean) => void,
@@ -189,19 +205,7 @@ const ChatText = (props: ChatTextProps, ref: Ref<{
                 </span>
               ) : null}
               {index === chatListRef.current.length - 1 && item.type === 'system' ? (
-                <svg
-                  className="cursor-pointer"
-                  onClick={continueChat}
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    fill="currentColor"
-                    d="M12 12.575q-.2 0-.375-.062T11.3 12.3L6.7 7.7q-.275-.275-.288-.688T6.7 6.3q.275-.275.7-.275t.7.275l3.9 3.875L15.9 6.3q.275-.275.688-.287t.712.287q.275.275.275.7t-.275.7l-4.6 4.6q-.15.15-.325.213t-.375.062Zm0 6q-.2 0-.375-.062T11.3 18.3l-4.6-4.6q-.275-.275-.288-.687T6.7 12.3q.275-.275.7-.275t.7.275l3.9 3.875l3.9-3.875q.275-.275.688-.288t.712.288q.275.275.275.7t-.275.7l-4.6 4.6q-.15.15-.325.213t-.375.062Z"
-                  />
-                </svg>
+                <ChatContinueSVG onClick={continueChat}/>
               ) : null}
             </div>
           </div>
